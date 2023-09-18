b25d2d3bd7848a98dddd8bd5cf46f1ae80be79f6
wahyu <handokowahyuh991@gmail.com>
2023-09-18 08:51:38 +0700

    tambah fitur baru db click

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const tombol = document.querySelector("button")
 const h1 = document.querySelector("h1")
+const body = document.body
 tombol.addEventListener('click', function(){
     if(h1.style.color == "red"){
         h1.style.color = "aqua"
     }else{
         h1.style.color = "red"
+    }
+})
+
+// double click feature
+tombol.addEventListener("dblclick", ()=>{
+    if(body.style.backgroundColor == "purple"){
+        body.style.backgroundColor = "white"
+    }else{
+        body.style.backgroundColor = "purple"
     }
 })
