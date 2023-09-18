f869f45c65587dbf59d842b7fc76c573baba50e1
wahyu <handokowahyuh991@gmail.com>
2023-09-18 09:06:12 +0700

    modify fitur

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const tombol = document.querySelector("button")
 const h1 = document.querySelector("h1")
-const body = document.body
 tombol.addEventListener('click', function(){
     if(h1.style.color == "red"){
         h1.style.color = "aqua"
     }else{
         h1.style.color = "red"
+    }
+})
+
+tombol.addEventListener("dblclick", ()=>{
+    if(body.style.backgroundColor == "purple"){
+        body.style.backgroundColor = "white"
+    }else{
+        body.style.backgroundColor = "purple"
     }
 })
